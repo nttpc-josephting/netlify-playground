cfa89cfce360d3b65e0997574f76adb4b1b59f58
Joseph Ting <j-ting@nttpc.co.jp>
2018-12-19 10:44:18 +0900

    feat: test netlify form

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,17 @@
       <p>This is to test some of the functions available on Netlify.</p>
       <p>Testing Netlify deploy previews.</p>
       <p>Split test: Master</p>
+      <form name="contact" method="POST" data-netlify="true">
+        <p>
+          <label>Email: <input type="email" name="email" /></label>
+        </p>
+        <p>
+          <label>Message: <textarea name="message"></textarea></label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
     </div>
     <p class="powered">Powered by Netlify</p>
   </div>
