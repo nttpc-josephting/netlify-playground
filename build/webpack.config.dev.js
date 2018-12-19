c4789af16d181a848735a22752b76fbffe49a0bb
Joseph Ting <j-ting@nttpc.co.jp>
2018-12-19 11:34:20 +0900

    feat: add _redirects to dist

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -2,6 +2,7 @@ const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   mode: 'development',
@@ -56,6 +57,10 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
-    })
+    }),
+    new CopyWebpackPlugin([{
+      from: 'static',
+      to: 'dist'
+    }])
   ]
 }
